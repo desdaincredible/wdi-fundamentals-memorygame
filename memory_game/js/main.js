815c8b4cd971ced9f221617931917e061d14335d
Destiny <dshillis1@gmail.com>
2019-03-02 11:08:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen.");
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king.");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You've found a match!");
+	}
+	else if (cardsInPlay[0] != cardsInPlay[1]) {
+		alert("Sorry, try again.")
+	}
+}
