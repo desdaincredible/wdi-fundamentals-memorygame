76f32db5a8e63e82b49a40a6b2183dde645dc3d0
Destiny <dshillis1@gmail.com>
2019-03-03 15:43:41 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -38,7 +38,8 @@ var checkForMatch = function () {
 }
 
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	cardId = this.getAttribute('data-id')[0];
 	
 console.log("User flipped " + cards[cardId].rank);
 
@@ -47,12 +48,25 @@ cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 
+this.setAttribute('src', cards[cardId].cardImage);
 
 if (cardsInPlay.length === 2) {
 	checkForMatch();
 }
 
 }
-flipCard(0);
-flipCard(2);
 
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', [i]);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+
+createBoard();
